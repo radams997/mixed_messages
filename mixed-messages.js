818a28cdd61dbdac3d22bb6a81902e0059a015fc
RebeccaAdams <radams997@gmail.com>
2024-02-20 14:04:50 -0500

    Added Comments to explain code

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,19 +1,26 @@
+// Storing three arrays in an object. Message is the object. sign, luck, and advice are they keys.
 let message = {
     sign: ['Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Saggitarius', 'Capricorn', 'Aquarius'],
     luck: ['good luck', 'bad luck'],
     advice: ['Trust yourself', 'Trust no one', 'Enjoy the present']
 };
 
+// Function with num as the parameter to create a random number to be passed into randomMessage
 const randomNum = num => {
     return Math.floor(Math.random() * num);
 }
 
+// Function to generate our message
 const randomMessage = () => {
+    //Array to store message
     let messageContent = [];
 
+    //Iterate through object 
     for (let key in message) {
+        // Takes randomNum function with parameter of our message object key length and saves it to optionIndex
         let optionIndex = randomNum(message[key].length);
 
+        //Generate Message
         if (key === 'sign') {
             messageContent.push(`Your sign is ${message[key][optionIndex]}.`);
         } else if (key === 'luck') {
@@ -22,7 +29,9 @@ const randomMessage = () => {
             messageContent.push(`You should ${message[key][optionIndex]}.`);
         }
     }
+    // Returns our joined message content array seperated by a space
     return messageContent.join(' ');
 };
 
+//Logs randomMessage
 console.log(randomMessage());
